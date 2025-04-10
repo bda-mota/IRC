d2e98f6ba6c54c78e7dc10b57d6a772eb09500a5
BiancaSantana81 <biancasantana81@hotmail.com>
2025-04-10 17:37:15 -0300

    done LISTC

--- srcs/commands/LISTC.cpp
+++ srcs/commands/LISTC.cpp
@@ -1,11 +1,34 @@
 #include "../../includes/irc.hpp"
 
 std::string CommandsArgs::listc(const std::vector<std::string>& args, Server& server, User* user) {
-    (void)server;
-    (void)user;
-    std::cout << "LISTC command executed!" << std::endl;
-    for (size_t i = 0; i < args.size(); i++) {
-        std::cout << "Arg " << i << ": " << args[i] << std::endl;
-    }
-    return "LISTC command executed!\r\n";
+	(void)args;
+
+	std::ostringstream response;
+
+	response << ":ircserver 321 " << user->getNickName() << " Channel :Users Name\r\n";
+
+	const std::map<std::string, Channel*>& channels = server.getChannels();
+
+    if (channels.empty())
+	{
+		std::string reply = ":ircserver 323 " + user->getNickName() + " :No channels available\r\n";
+		send(user->getFd(), reply.c_str(), reply.length(), 0);
+		return "";
+	}
+
+	for (std::map<std::string, Channel*>::const_iterator it = channels.begin(); it != channels.end(); ++it)
+	{
+		std::string channelName = it->first;
+		Channel* channel = it->second;
+		size_t numUsers = channel->getUsers().size();
+
+		response << ":ircserver 322 " << user->getNickName() << " "
+				 << channelName << " " << numUsers << " :" << channel->getTopic() << "\r\n";
+	}
+    
+	response << ":ircserver 323 " << user->getNickName() << " :End of /LISTC\r\n";
+
+	send(user->getFd(), response.str().c_str(), response.str().length(), 0);
+	
+	return "";
 }
