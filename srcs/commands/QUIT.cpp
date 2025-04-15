877219bab31d10d6b973f05af9a99a3665b3c99a
BiancaSantana81 <biancasantana81@hotmail.com>
2025-04-15 15:52:37 -0300

    ajustando mensagem de QUIT e PART para enviar para todos os users do canal

--- srcs/commands/QUIT.cpp
+++ srcs/commands/QUIT.cpp
@@ -1,11 +1,38 @@
 #include "../../includes/irc.hpp"
 
 std::string CommandsArgs::quit(const std::vector<std::string>& args, Server& server, User* user) {
-    (void)server;
-    (void)user;
-    std::cout << "QUIT command executed!" << std::endl;
-    for (size_t i = 0; i < args.size(); i++) {
-        std::cout << "Arg " << i << ": " << args[i] << std::endl;
-    }
-    return "";
+	std::string reason;
+
+	if (!args.empty()) {
+		for (size_t i = 0; i < args.size(); i++) {
+			if (i == 0)
+				reason = args[i];
+			else
+				reason += " " + args[i];
+		}
+	} else {
+		reason = "Client Quit";
+	}
+
+	std::ostringstream quitMsg;
+	quitMsg << ":" << user->getNickName() << "!" << user->getUserName()
+	        << "@" << user->getHostName() << " QUIT :" << reason << "\r\n";
+
+	std::vector<Channel*> joinedChannels = user->getJoinedChannels();
+
+	for (size_t i = 0; i < joinedChannels.size(); ++i) {
+		Channel* channel = joinedChannels[i];
+		channel->sendToAll(quitMsg.str());
+		channel->removeUser(user->getFd());
+
+		if (channel->getUsers().empty()) {
+			server.getChannels().erase(channel->getName());
+			delete channel;
+		}
+	}
+
+	server.clearUsers(user->getFd());
+	close(user->getFd());
+
+	return "";
 }
