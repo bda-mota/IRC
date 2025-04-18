5a52922f52b1b01ae9ffd1d3a8a32231bdced4d0
BiancaSantana81 <biancasantana81@hotmail.com>
2025-04-18 16:04:22 -0300

    refatoração de USER

--- srcs/commands/USER.cpp
+++ srcs/commands/USER.cpp
@@ -1,60 +1,55 @@
 #include "../../includes/irc.hpp"
 
 static void auxBuildRealname(std::string& realname, const std::vector<std::string>& args, size_t start);
+static std::string getClientHostname(int fd);
 
 std::string CommandsArgs::user(const std::vector<std::string>& args, Server& server, User* user) {
-	(void)server; // TODO: manter server aqui? está sem uso
-    user->setHasUserCommand(true);
+	(void)server; // Unused parameter
 
-    struct sockaddr_in addr;
-    socklen_t addr_len = sizeof(addr);
-
-    if (getpeername(user->getFd(), (struct sockaddr*)&addr, &addr_len) == 0) {
-	    std::string hostname = inet_ntoa(addr.sin_addr);
-	    user->setHostName(hostname);
-    }
+	std::string hostname = getClientHostname(user->getFd());
+	if (!hostname.empty())
+		user->setHostName(hostname);
 
 	if (args.size() < 4) {
-        std::string error = ":ircserver 461 " + user->getNickName() + " USER :Not enough parameters\r\n";
-        send(user->getFd(), error.c_str(), error.length(), 0);
+		sendError(user, ERR_NEEDMOREPARAMS("USER", "Not enough parameters"));
 		return "";
 	}
 
     if (user->getRegistered())
     {
-        std::string error = ":ircserver 462 " + user->getNickName() + " :You may not reregister\r\n";
-		send(user->getFd(), error.c_str(), error.length(), 0);
+        sendError(user, ERR_ALREADYREGISTERED(user->getNickName()));
 		return "";
     }
 
 	user->setUserName(args[0]);
-    std::string hostname = args[1]; // hostname or IP address não é usado para nada no protocolo, mas capturei caso a gnt precise usar depois
-    user->setServerName(args[2]); // server name idem hostname
 	std::string realname;
 
-	if (args.size() >= 4 && args[3][0] == ':')
-		auxBuildRealname(realname, args, 3);
-	else
-    {
-        std::string error = ":ircserver 461 " + user->getNickName() + " USER :Missing realname\r\n";
-        send(user->getFd(), error.c_str(), error.length(), 0);
+	if (args[3][0] != ':') {
+        sendError(user, ERR_NEEDMOREPARAMS("USER", "Missing realname"));
         return "";
-    }
-
-	if (!user->getRegistered()) {
-		sendWelcomeMessage(user);
 	}
+	auxBuildRealname(realname, args, 3);
+
+	if (!user->getRegistered())
+		sendWelcomeMessage(user);
 
     user->setRealName(realname);
 
-	return ""; // os retornos saem no hexchat
+	return "";
 }
 
 static void auxBuildRealname(std::string& realname, const std::vector<std::string>& args, size_t start) {
-	for (size_t i = start; i < args.size(); i++) {
-		if (i == start)
-			realname = args[i].substr(1);
-		else
-			realname += " " + args[i];
-	}
+	std::ostringstream oss;
+	oss << args[start].substr(1);
+	for (size_t i = start + 1; i < args.size(); ++i)
+		oss << ' ' << args[i];
+	realname = oss.str();
+}
+
+static std::string getClientHostname(int fd) {
+	struct sockaddr_in addr;
+	socklen_t addr_len = sizeof(addr);
+	if (getpeername(fd, (struct sockaddr*)&addr, &addr_len) == 0)
+		return inet_ntoa(addr.sin_addr);
+	return "";
 }
