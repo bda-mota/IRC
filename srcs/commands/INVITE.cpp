fdc936f3d6427fe54580981f43e0c8940aeaa428
BiancaSantana81 <biancasantana81@hotmail.com>
2025-04-18 16:22:54 -0300

    refatorando INVITE

--- srcs/commands/INVITE.cpp
+++ srcs/commands/INVITE.cpp
@@ -1,53 +1,36 @@
 #include "../../includes/irc.hpp"
 
-/*
-    Parameters: <nickname> <channel>
-*/
-
 static User* findUserInServer(Server& server, User* sender, const std::string& targetNick);
 static Channel* findChannelInServer(Server& server, User* sender, const std::string& channelName);
+static void sendInviteMessages(User* from, User* to, const std::string& channelName);
 
 std::string CommandsArgs::invite(const std::vector<std::string>& args, Server& server, User* user) {
 
-    std::cout << "invite command called" << std::endl;
-
      if (args.size() < 2) {
-        std::string error = ":ircserver 461 " + user->getNickName() + " INVITE :Not enough parameters\r\n";
-        send(user->getFd(), error.c_str(), error.length(), 0);
+        sendError(user, ERR_NEEDMOREPARAMS("INVITE", "Not enough parameters"));
         return "";
     }
 
     std::string targetNick = args[0];
     std::string channelName = args[1];
 
-    // Procurando usuário no servidor para ser adicionado no channel
     User* targetUser = findUserInServer(server, user, targetNick);
     if (!targetUser)
         return "";
 
-    // Verificando se o canal existe
     Channel* channel = findChannelInServer(server, user, channelName);
     if (!channel)
         return "";
 
     // TODO: verificar se o usuário é operador do canal e se é um canal privado após a implementação do MODE
 
-    // Verificando se o usuário que faz o convite está no canal
     if (!channel->isUserInChannel(user)) {
-        std::string error = ":ircserver 442 " + user->getNickName() + " " + channelName + " :You're not on that channel\r\n";
-        send(user->getFd(), error.c_str(), error.length(), 0);
+        sendError(user, ERR_NOTONCHANNEL(channelName));
         return "";
     }
 
     targetUser->addInvitation(channelName);
-
-    // Enviando mensagem de convite para o usuário
-    std::string inviteMessage = ":ircserver INVITE " + targetUser->getNickName() + " " + channelName + "\r\n";
-    send(targetUser->getFd(), inviteMessage.c_str(), inviteMessage.length(), 0);
-
-    // Enviando mensagem de confirmação para o usuário que fez o convite
-    std::string confirmation = ":ircserver 341 " + user->getNickName() + " " + targetNick + " " + channelName + " :Invite sent successfully\r\n";
-    send(user->getFd(), confirmation.c_str(), confirmation.length(), 0);
+    sendInviteMessages(user, targetUser, channelName);
 
     return "";
 }
@@ -61,9 +44,7 @@ static User* findUserInServer(Server& server, User* sender, const std::string& t
         }
     }
 
-    // Usuário não encontrado: envia erro 401
-    std::string error = ":ircserver 401 " + sender->getNickName() + " " + targetNick + " :No such nick/channel\r\n";
-    send(sender->getFd(), error.c_str(), error.length(), 0);
+    sendError(sender, ERR_NOSUCHNICK(targetNick));
     return NULL;
 }
 
@@ -74,7 +55,11 @@ static Channel* findChannelInServer(Server& server, User* sender, const std::str
         return channels[channelName];
     }
 
-    std::string error = ":ircserver 403 " + sender->getNickName() + " " + channelName + " :No such channel\r\n";
-    send(sender->getFd(), error.c_str(), error.length(), 0);
+    sendError(sender, ERR_NOSUCHCHANNEL(channelName));
     return NULL;
+}
+
+static void sendInviteMessages(User* from, User* to, const std::string& channelName) {
+	sendResponse(to, RPL_INVITEMSG(from->getNickName(), from->getUserName(), to->getNickName(), channelName));
+	sendResponse(from, RPL_INVITING(from->getNickName(), to->getNickName(), channelName));
 }
