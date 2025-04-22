53b87adc95ef80b11a07e459f5b6c6425fb18e54
BiancaSantana81 <biancasantana81@hotmail.com>
2025-04-22 16:46:50 -0300

    update KICK -> mudanças para args de KICK

--- srcs/commands/KICK.cpp
+++ srcs/commands/KICK.cpp
@@ -5,17 +5,40 @@
 */
 
 static void buildTrailingMessage(std::string& message, const std::vector<std::string>& args, size_t start);
-static bool extractKickParams(std::string& channelName, std::string& targetNick, std::string& reason, const std::vector<std::string>& args);
+//static bool extractKickParams(std::string& channelName, std::string& targetNick, std::string& reason, const std::vector<std::string>& args);
 
 std::string CommandsArgs::kick(const std::vector<std::string>& args, Server& server, User* user) {
 
-    std::string channelName, targetNick, reason;
-	if (!extractKickParams(channelName, targetNick, reason, args)) {
+	if (args.size() < 4) {
 		sendError(user, ERR_NEEDMOREPARAMS(user->getNickName(), "KICK"));
 		return "";
 	}
 
-    std::cout << "KICK: " << channelName << " | " << targetNick << " | " << reason << std::endl;
+	std::string channelName = args[1];
+	if (channelName[0] != '#') {
+		sendError(user, ERR_NEEDMOREPARAMS(user->getNickName(), "KICK")); // ou erro custom
+		return "";
+	}
+
+	std::cout << "channelName: " << channelName << std::endl;
+
+	std::string targetNick = args[2];
+	if (targetNick[0] == ':')
+		targetNick = targetNick.substr(1);  // Corrige casos em que nick vem com ':'
+
+	std::cout << "targetNick: " << targetNick << std::endl;
+
+	std::string reason;
+	if (args.size() >= 4) {
+		if (args[3][0] == ':') {
+			buildTrailingMessage(reason, args, 3);
+		} else {
+			reason = args[3];
+			for (size_t i = 4; i < args.size(); ++i)
+				reason += ' ' + args[i];
+		}
+	}
+	std::cout << "reason: " << reason << std::endl;
 
     Channel* channel = findChannelInServer(server, user, channelName);
     if (!channel)
@@ -59,27 +82,25 @@ static void buildTrailingMessage(std::string& message, const std::vector<std::st
 	message = oss.str();
 }
 
-static bool extractKickParams(std::string& channelName, std::string& targetNick, std::string& reason, const std::vector<std::string>& args) {
-	size_t startIndex = 0;
-	while (startIndex < args.size() && args[startIndex][0] != '#') {
-		++startIndex;
-	}
+// static bool extractKickParams(std::string& channelName, std::string& targetNick, std::string& reason, const std::vector<std::string>& args) {
+// 	if (args.size() < 3)
+// 		return false;
 
-	if (args.size() - startIndex < 2 || args[startIndex][0] != '#') {
-		return false;
-	}
+// 	channelName = args[0];
+// 	if (channelName[0] != '#')
+// 		return false;
 
-	channelName = args[startIndex];
+// 	targetNick = args[1];
+// 	if (targetNick[0] == ':')
+// 		targetNick = targetNick.substr(1);
 
-	std::string rawNick = args[startIndex + 1];
-	targetNick = (rawNick[0] == ':') ? rawNick.substr(1) : rawNick;
+// 	if (args[2][0] == ':') {
+// 		buildTrailingMessage(reason, args, 2);
+// 	} else {
+// 		reason = args[2];
+// 		for (size_t i = 3; i < args.size(); ++i)
+// 			reason += ' ' + args[i];
+// 	}
 
-	reason.clear();
-	if (args.size() > startIndex + 2 && args[startIndex + 2][0] == ':') {
-		buildTrailingMessage(reason, args, startIndex + 2);
-	} else if (args.size() > startIndex + 2) {
-		reason = args[startIndex + 2];
-	}
-
-	return true;
-}
+// 	return true;
+// }
